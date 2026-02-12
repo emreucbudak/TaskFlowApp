9b42b4710a9407fdd89fafbb01083f85ff5510db
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-12 03:14:59 +0300

    Proje dosyası ekle.

---
+++ TaskFlowApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaskFlowApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
